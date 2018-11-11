d80c7f600cfb1c2c4c171173dcdc22b9918fe36c
Cristiano Napoleao <cristianojorgeapple0994@Cristianos-MacBook-Pro.local>
2018-11-10 19:48:27 -0600

    UI update for Felix

--- application.py
+++ application.py
@@ -1,9 +1,11 @@
-from flask import Flask, request,render_template,url_for, redirect
+from flask import Flask, request,render_template,url_for,redirect, session
+import os
 from accounts import *
 from bank import *
 from atm import *
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.urandom(16)
 
 bank = Bank()
 account = Account
@@ -12,10 +14,12 @@ atm = ATM
 @app.route('/', methods = ['POST', 'GET'])
 def index():
 
-    accountNumber = request.form.get("acctNum")
+    session['accountNumber'] = None
 
     if request.method == 'POST':
-        if bank.getCheckingAccountInfo(accountNumber) == None:
+        session['accountNumber'] = request.form.get("acctNum")
+
+        if bank.getCheckingAccountInfo(session.get('accountNumber')) == None:
             return redirect(url_for ('noAccount'))
         else:
             return redirect(url_for ('accountInfo'))
@@ -24,12 +28,28 @@ def index():
 
 @app.route('/accountInformation', methods = ['GET', 'POST'])
 def accountInfo():
-    accountNumber = request.form.get("acctNum")
+
+    accountNumber = session.get('accountNumber')
     c_account = bank.getCheckingAccountInfo(accountNumber)
     s_account = bank.getSavingsAccountInfo(accountNumber)
+
+    """Gets the account number from main page and
+    verifies the existence of the account, if true, passes the values to template"""
+
     if c_account == None and s_account == None:
         return redirect(url_for ('noAccount'))
+
     else:
+
+        """ Enabling user to take actions upon accounts from UI"""
+        if request.method == "POST":
+            action = request.form.get("actions")
+            if c_account != None:
+                if action == "blockAccount":
+                    c_account._block = True
+                elif action == "unblockAccount":
+                    c_account._block = False
+
         if c_account != None:
             clientName = c_account._name
             accountType = c_account._accountType
@@ -51,10 +71,12 @@ def accountInfo():
                 status = "Blocked"
             value = s_account._balance
             balance = "$ {:,.2f}".format(value)
-            return render_template("accountInfoView.html", accountNumber = accountNumber, clientName = clientName, accountType = accountType, status = status, balance = balance)
-# @app.route('/about')
-# def about():
-#     return render_template("about.html")
+
+    return render_template("accountInfoView.html", accountNumber = accountNumber, clientName = clientName, accountType = accountType, status = status, balance = balance)
+
+@app.route('/noAccount')
+def noAccount():
+    return render_template("noAccount.html")
 
 # @app.route("/addchecking.html", method=['GET', 'POST'])
 # def add_cheking():
