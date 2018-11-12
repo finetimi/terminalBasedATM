398a8b51cadb1d5c762285af66b391c3f832c4cc
Cristiano Napoleao <cristianojorgeapple0994@Cristianos-MacBook-Pro.local>
2018-11-12 02:22:11 -0600

    UI second phase Manager Actions complete

--- application.py
+++ application.py
@@ -9,16 +9,17 @@ app.config['SECRET_KEY'] = os.urandom(16)
 
 bank = Bank()
 account = Account
+s_account = SavingsAccount
 atm = ATM
 
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-
+    session.pop('accountNumber', None)
     session['accountNumber'] = None
 
     if request.method == 'POST':
         session['accountNumber'] = request.form.get("acctNum")
-        if bank.getCheckingAccountInfo(session.get('accountNumber', None)) == None:
+        if bank.getCheckingAccountInfo(session.get('accountNumber', None)) == None and bank.getSavingsAccountInfo(session.get('accountNumber', None)) == None:
             return redirect(url_for ('noAccount'))
         else:
             return redirect(url_for ('accountInfo'))
@@ -48,6 +49,23 @@ def accountInfo():
                     c_account._block = True
                 elif action == "unblockAccount":
                     c_account._block = False
+                elif action == "delete_c_Account":
+                    bank.removeChecking(accountNumber)
+                    bank.saveCheking("c_accounts.txt")
+                    return redirect(url_for('index'))
+
+            elif s_account != None:
+                if action == "blockAccount":
+                    s_account._block = True
+                elif action == "unblockAccount":
+                    s_account._block = False
+                elif action == "resetSavings":
+                    s_account.reset()
+                elif action == "delete_s_Account":
+                    bank.removeSavings(accountNumber)
+                    bank.saveSavings("s_accounts.txt")
+                    return redirect(url_for('index'))
+
 
         if c_account != None:
             clientName = c_account._name
@@ -59,6 +77,7 @@ def accountInfo():
                 status = "Blocked"
             value = c_account._balance
             balance = "$ {:,.2f}".format(value)
+            bank.saveCheking("c_accounts.txt")
             return render_template("accountInfoView.html", accountNumber = accountNumber, clientName = clientName, accountType = accountType, status = status, balance = balance)
         elif s_account != None:
             clientName = s_account._name
@@ -70,16 +89,36 @@ def accountInfo():
                 status = "Blocked"
             value = s_account._balance
             balance = "$ {:,.2f}".format(value)
-
+            bank.saveSavings("s_accounts.txt")
     return render_template("accountInfoView.html", accountNumber = accountNumber, clientName = clientName, accountType = accountType, status = status, balance = balance)
 
 @app.route('/noAccount')
 def noAccount():
     return render_template("noAccount.html")
 
-# @app.route("/addchecking.html", method=['GET', 'POST'])
-# def add_cheking():
+@app.route("/addChecking", methods = ['GET', 'POST'])
+def add_cheking():
+    session.pop('accountNumber', None)
+    if request.method == 'POST':
+        fullName = request.form.get('fullName')
+        newChecking = account(fullName)
+        session['accountNumber'] = newChecking.getAccountNumber()
+        bank.addChecking(newChecking)
+        bank.saveCheking("c_accounts.txt")
+        return redirect( url_for('accountInfo') )
+    return render_template("addchecking.html")
 
+@app.route("/addSavings", methods = ['GET', 'POST'])
+def add_savings():
+    session.pop('accountNumber', None)
+    if request.method == 'POST':
+        fullName = request.form.get('fullName')
+        newSavings = s_account(fullName)
+        session['accountNumber'] = newSavings.getAccountNumber()
+        bank.addSavings(newSavings)
+        bank.saveSavings("s_accounts.txt")
+        return redirect( url_for('accountInfo') )
+    return render_template("addsavings.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
